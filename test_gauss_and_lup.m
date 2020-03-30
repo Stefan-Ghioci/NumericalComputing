0b4f897c7d035809d74ee90b858282dbd2447ddf
Stefan Ghioci <ghioci.stefan@gmail.com>
2020-03-31 02:45:46 +0300

    Improve test

--- test_gauss_and_lup.m
+++ test_gauss_and_lup.m
@@ -2,11 +2,11 @@ function test_gauss_and_lup()
 format long;
 
 disp ("Solving equation-system:")
-A = [2 1 -1; -3 -1 2; -2 1 2];
-b = [8; -11; -3];
+A=rand(4);
+b=sum(A,2);
 
-for i=1:3
-    disp(A(i,1) + "*x + " + A(i,2) +  "*y + " + A(i,3) + "*z = " + b(i));
+for i=1:4
+    disp(A(i,1) + "*x + " + A(i,2) +  "*y + " + A(i,3) + "*z +" + A(i,4) +"*t = " + b(i));
 end
 
 disp("Matrix division solution:");
