87fa5df567603a1cb372c2db94afc23acd3d2eca
Stefan Ghioci <ghioci.stefan@gmail.com>
2020-03-30 21:40:59 +0300

    Add test function for Gauss Elimination

---
+++ test_gauss_elim.m
@@ -0,0 +1,18 @@
+function test_gauss_elim()
+format long;
+
+disp ("Solving equation-system:")
+A = [2 1 -1; -3 -1 2; -2 1 2];
+b = [8; -11; -3];
+
+for i=1:3
+    disp(A(i,1) + "*x + " + A(i,2) +  "*y + " + A(i,3) + "*z = " + b(i));
+end
+
+disp("Matrix division solution:");
+disp(A\b);
+disp("Gaussian Elimination solution:");
+disp(gauss_elim([A b]));
+
+format short;
+end
